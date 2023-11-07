ed43c011e48934e2016f564f7e85c3a5377f67d9
Delano Oliveira <delanohelio@gmail.com>
2023-11-07 14:58:32 -0300

    Adding eco_energy and future_city

--- app.py
+++ app.py
@@ -22,5 +22,19 @@ def consulta_decisiva():
             }
     return data
 
+@app.route('/future_city', methods=['GET'])
+def future_city():
+    data = {"co2_mes": random.randint(0, 200000),
+            "co2_evitado": random.randint(0, 50000),
+            }
+    return data
+
+@app.route('/eco_energy', methods=['GET'])
+def eco_energy():
+    data = {"kw_mes": random.randint(0, 500),
+            "kw_atual": random.randint(0, 10),
+            }
+    return data
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
