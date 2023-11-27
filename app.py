3f474aa12ddabc8843339897466127a1532f2ee5
Delano Oliveira <delanohelio@gmail.com>
2023-11-27 11:24:13 -0300

    Update API

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import random
 
 app = Flask(__name__)
 CORS(app)
+app.json.ensure_ascii = False
+
+import project_terapias
+from produto_SBJL import projetos, dados_do_formulario_lista
 
 @app.route('/')
 def OK():
     return 'OK'
 
-#total de vendas
-#lucro
-#distribuição
-#clientes atendidos
+############# Consulta Decisiva ################
 @app.route('/consulta_decisiva', methods=['GET'])
 def consulta_decisiva():
     data = {"total_de_vendas": random.randint(0, 200000),
@@ -22,6 +23,7 @@ def consulta_decisiva():
             }
     return data
 
+############# Future City ################
 @app.route('/future_city', methods=['GET'])
 def future_city():
     data = {"co2_mes": random.randint(0, 200000),
@@ -29,12 +31,91 @@ def future_city():
             }
     return data
 
+############# Eco Energy ################
 @app.route('/eco_energy', methods=['GET'])
 def eco_energy():
     data = {"kw_mes": random.randint(0, 500),
             "kw_atual": random.randint(0, 10),
             }
     return data
+
+############# Projeto Terapias ################
+@app.route('/terapia_depoimentos', methods=['GET'])
+def get_terapia_depoimentos():
+    return project_terapias.terapia_depoimentos
+
+@app.route('/terapia_depoimentos', methods=['POST'])
+def add_terapia_depoimento():
+    try:
+        depoimento = request.get_json()
+        tipo_terapia = depoimento.get('tipo_terapia')
+        if (tipo_terapia in project_terapias.terapia_depoimentos):
+            depoimentos = project_terapias.terapia_depoimentos[tipo_terapia]
+            depoimentos.append(
+                {
+                    "nome": depoimento.get("nome"),
+                    "sobrenome": depoimento.get("sobrenome"),
+                    "idade": depoimento.get("idade"),
+                    "email": depoimento.get("email"),
+                    "tratamento": depoimento.get("tratamento")
+                }
+            )
+            return "Depoimento adicionado com sucesso"
+        else:
+            return jsonify({"error": "nao existe esse tipo de terapia"}), 400
+    except Exception as e:
+        print(f"Erro interno: {e}")
+        return jsonify({"error": "Erro interno no servidor"}), 500
+
+############# Projeto SBJL ################
+@app.route('/paginaProjeto/<index>', methods=['GET'])
+def projeto(index):
+    return projetos[int(index)]
+
+@app.route('/adicionar_comentario', methods=['POST'])
+def adicionar_comentario():
+    try:
+        if request.is_json:
+            data = request.get_json()
+
+            numero = data.get('numero')
+            if numero is None:
+                return jsonify({"error": "O campo 'numero' é obrigatório"}), 400
+            else:
+                numero = int(numero)
+    
+            comentario = {
+                "nome": data.get('nome'),
+                "comentario": data.get('comentario')
+            }
+
+            if (numero >= 0 and numero < len(projetos)):
+                projeto = projetos[numero]
+                projeto["comentarios"].append(comentario)
+            else:
+                return jsonify({"error": "o numero nao esta dentro da lista de projetos"}), 400
+
+            return jsonify({"message": "Comentário recebido e salvo com sucesso"}), 200
+        else:
+            return jsonify({"error": "A solicitação deve conter dados JSON"}), 400
+    except Exception as e:
+        print(f"Erro interno: {e}")
+        return jsonify({"error": "Erro interno no servidor"}), 500
+
+@app.route('/enviarFormulario', methods=['POST'])
+def enviarFormulario():
+    try:
+        dados_do_formulario = request.get_json()
+        dados_do_formulario_lista.append(dados_do_formulario)
+
+        print("Lista de dados do formulário:")
+        print(dados_do_formulario_lista)
+
+        return jsonify({"message": "Dados do formulário recebidos com sucesso!"}), 200
+
+    except Exception as e:
+        print(f"Erro interno: {e}")
+        return jsonify({"error": "Erro interno no servidor"}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
---
+++ project_terapias.py
@@ -0,0 +1,54 @@
+terapia_depoimentos = {
+        "Medula_Espinhal":
+            [
+                {
+                    "nome": "Ana",
+                    "sobrenome": "L.",
+                    "idade": "",
+                    "email": "",
+                    "tratamento": "Ana sofreu uma lesão traumática na medula espinhal em um acidente de carro. Após a terapia com células-tronco, ela experimentou uma notável recuperação da função motora e sensorial. Hoje, ela caminha com o auxílio de dispositivos de mobilidade e compartilha sua história de superação."
+                },
+
+            ],
+        "Doenças_Cardiacas":
+            [
+                {
+                    "nome": "Laura",
+                    "sobrenome": "R.",
+                    "idade": "",
+                    "email": "",
+                    "tratamento": "Laura lutou contra uma doença cardíaca crônica que a obrigava a tomar vários medicamentos diariamente. Após a terapia com células-tronco, sua função cardíaca melhorou significativamente, permitindo que ela reduzisse a quantidade de medicamentos. Agora, ela se sente mais energética e saudável."
+                },
+
+            ],
+        "Diabetes_Tipo_1": [
+            {
+                "nome": "Carlos",
+                "sobrenome": "M.",
+                "idade": "",
+                "email": "",
+                "tratamento": "Carlos foi diagnosticado com diabetes tipo 1 em uma idade jovem e dependia de injeções diárias de insulina. Após a terapia com células-tronco, seu pâncreas começou a produzir insulina de forma mais eficiente. Agora, ele mantém níveis glicêmicos mais estáveis e tem uma vida ativa."
+            },
+
+        ],
+        "Problemas_Oculares": [
+            {
+                "nome": "Maria",
+                "sobrenome": "S.",
+                "idade": "",
+                "email": "",
+                "tratamento": "Maria sofria de degeneração macular, uma condição que ameaçava sua visão. Após o tratamento com células-tronco, ela experimentou uma melhora significativa em sua visão. Hoje, ela desfruta de uma vida mais independente e gratificante"
+            },
+
+        ],
+        "Cancer_de_Pulmão": [
+            {
+                "nome": "João",
+                "sobrenome": "C.",
+                "idade": "",
+                "email": "",
+                "tratamento": "João passou por uma cirurgia para tratar o câncer de pulmão. A terapia com células-tronco foi usada como parte de seu plano de recuperação. Ele se recuperou mais rapidamente e com menos complicações pós-operatórias, o que o deixou mais forte para enfrentar sua jornada de cura."
+            },
+
+        ]
+    }
